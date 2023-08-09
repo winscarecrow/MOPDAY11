0fd9041ad3417b1dc2e293bce0f72517274d00e5
winscarecrow <55477560+winscarecrow@users.noreply.github.com>
2023-08-09 20:40:44 +0700

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,14 @@
-print("My first program")
-print("My name is Win")
-print("From My Program")
-print("Bye Bye")
-if 5 > 2:
-    print("Five is greater than two!")
+def grade_calculate(score):
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+    
+print(grade_calculate(85))
+print(grade_calculate(95))
